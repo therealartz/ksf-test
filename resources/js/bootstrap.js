2acc8962d153a72c38993fab1d502b2c955f7df1
Artem Zakharchenko <artz.relax@gmail.com>
2019-05-29 21:25:43 +0300

    Remove csrf token

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,28 +3,4 @@
  * for JavaScript based Bootstrap features such as modals and tabs. This
  * code may be modified to fit the specific needs of your application.
  */
-
 $ = jQuery = require('jquery');
-
-/**
- * Next we will register the CSRF Token as a common header with JQuery AJAX so that
- * all outgoing HTTP requests automatically have it attached. This is just
- * a simple convenience so we don't have to attach every token manually.
- */
-
-let token = document.head.querySelector('meta[name="csrf-token"]');
-
-if (token) {
-  /**
-   * Setting CSRF token header for ajax requests
-   */
-  $.ajaxSetup({
-    headers: {
-      'X-CSRF-TOKEN': token.getAttribute('content'),
-      'Accept': 'application/json'
-    }
-  });
-
-} else {
-  console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', config('app.name', 'Feedback Form'))</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}"/>
 </head>
